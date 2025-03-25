de314433b8c9c56a0a578ea4f78349ca7426c056
average <julianmuchacho666@gmail.com>
2025-03-25 01:47:15 -0400

    Update nodemap.ts

--- src/nodemap.ts
+++ src/nodemap.ts
@@ -159,6 +159,7 @@ class NodeMap {
                     : { nodes: KnotNode[], currentNode: string[], lastStart: number, lastName: string | null, isFunction }
                 , line: string
                 , index: number) => {
+                if (line.match(/^\s*===(\s*function)?\s*(\w+)\s*===/)) {
                     // Found the start of a new knot.
 
                     const match = line.match(/^\s*===(\s*function)?\s*(\w+)\s*===/);
